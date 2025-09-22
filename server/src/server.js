fced6041c503e7dc2e95dca98c21d4fe7a894f17
DhavalMalsattar <122300767+DhavalMalsattar@users.noreply.github.com>
2025-09-22 11:19:02 +0530

    env change

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,13 @@
 const app = require('./app');
 const { initDBConnections } = require('./db');
 const config = require('./config');
+const dotenv = require('dotenv');
+
+// dotenv.config({ path: path.resolve(__dirname, '../.env') });
+// dotenv.config();
+// dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
 
 async function start() {
   await initDBConnections();
